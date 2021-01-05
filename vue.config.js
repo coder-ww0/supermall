e82b6694e3658e743f0220a8d3a295dc5dba7f45
coder-ww0 <15027128561@163.com>
2021-01-05 14:07:03 +0800

    '首页开发01'

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  configureWebpack:{
+    resolve:{
+      alias:{
+        'asstes':'@/asstes',
+        'common':'@/common',
+        'components':'@/components',
+        'network':'@/network'
+
+
+      }
+    }
+  }
+}
